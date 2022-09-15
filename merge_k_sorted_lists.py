8a555edbe8165fbdc819e40654237661f992320e
Kostas Tingos <kostastingos@college.harvard.edu>
2022-09-14 21:31:26 -0400

    Fix linter errors for naming, spacing, etc
    There had been several linter errors for function naming, line spacing,
    etc. Now those have been addressed.

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -1,44 +1,51 @@
-# define the ListNode class
+"""Solution for the merge k sorted lists problem on leetcode"""
+
+
 class ListNode:
+    """ListNode class"""
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-# function that converts an array to a list
-def arrToList(arr):
+
+def arr_to_list(arr):
+    """converts an array to a list"""
     if len(arr) == 0:
         return None
-    return ListNode(arr[0],arrToList(arr[1:]))
+    return ListNode(arr[0], arr_to_list(arr[1:]))
 
-# function that converts a list to an array
-def listToArr(l):
-    if l is None:
+
+def list_to_arr(lst):
+    """converts a list to an array"""
+    if lst is None:
         return []
-    return [l.val] + listToArr(l.next)
+    return [lst.val] + list_to_arr(lst.next)
 
-# function that merges two sorted lists
-def mergeTwoLists(l1, l2):
+
+def merge_two_lists(lst1, lst2):
+    """merges two sorted lists"""
     start = ListNode()
     pointer = start
-    
-    while l1 and l2:
-        if l1.val < l2.val:
-            pointer.next = l1
-            l1 = l1.next
+
+    while lst1 and lst2:
+        if lst1.val < lst2.val:
+            pointer.next = list1
+            lst1 = lst1.next
         else:
-            pointer.next = l2
-            l2 = l2.next
+            pointer.next = list2
+            lst2 = lst2.next
         pointer = pointer.next
-        
-    if not l1:
-        pointer.next = l2
+
+    if not list1:
+        pointer.next = lst2
     else:
-        pointer.next = l1
-        
+        pointer.next = lst1
+
     return start.next
 
-list1 = arrToList([1,4,5])
-list2 = arrToList([1,3,4])
-list3 = arrToList([2,6])
 
-print(listToArr(mergeTwoLists(list1,list2)))
+list1 = arr_to_list([1, 4, 5])
+list2 = arr_to_list([1, 3, 4])
+list3 = arr_to_list([2, 6])
+
+print(list_to_arr(merge_two_lists(list1, list2)))
