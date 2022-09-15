2a0873054af0dffc7b152775fc6682b4861bbb06
Kostas Tingos <kostastingos@college.harvard.edu>
2022-09-14 20:40:39 -0400

    Add function that merges two lists
    As part of the overall solution to merge k lists, we start by creating a
    function that will merge two sorted lists.

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -3,3 +3,23 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+# function that merges two sorted lists
+def mergeTwoLists(l1, l2):
+    start = ListNode()
+    pointer = start
+    
+    while l1 and l2:
+        if l1.val < l2.val:
+            pointer.next = l1
+            l1 = l1.next
+        else:
+            pointer.next = l2
+            l2 = l2.next
+        
+    if not l1:
+        pointer.next = l2
+    else:
+        pointer.next = l1
+        
+    return start.next
